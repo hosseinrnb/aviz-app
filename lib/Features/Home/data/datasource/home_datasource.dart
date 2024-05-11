3e96a9a6bf577154d567de6c5b25e2c8236533f8
Hossein Ravanbakhsh <hosseinravanbakhsh.79@gmail.com>
2024-05-12 00:18:15 +0330

    some changes

--- lib/Features/Home/data/datasource/home_datasource.dart
+++ lib/Features/Home/data/datasource/home_datasource.dart
@@ -1,3 +1,30 @@
-abstract class IHomeDatasource{
+import 'package:aviz_application/Features/Home/data/model/promotion.dart';
+import 'package:aviz_application/NetworkUtil/api_exception.dart';
+import 'package:dio/dio.dart';
 
+abstract class IHomeDatasource {
+  Future<List<Promotion>> getHotPromotions();
+}
+
+class HomeRemoteDatasource extends IHomeDatasource {
+  final Dio _dio;
+
+  HomeRemoteDatasource(this._dio);
+
+  @override
+  Future<List<Promotion>> getHotPromotions() async {
+    try {
+      Map<String, dynamic> promotionQuery = {'filter': 'is_hot=true'};
+      var response = await _dio.get('collections/promotion/records',
+          queryParameters: promotionQuery);
+      return response.data['items']
+          .map<Promotion>((jsonObject) => Promotion.fromJson(jsonObject))
+          .toList();
+    } on DioException catch (dioException) {
+      throw ApiException(
+          dioException.response?.statusCode ?? 0, dioException.message);
+    } catch (ex) {
+      throw ApiException(0, 'unknown');
+    }
+  }
 }
